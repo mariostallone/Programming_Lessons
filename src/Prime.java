18cd819da3526d492f7f3bcb58abd8084c328a7b
Mario Stallone <mariostallone@gmail.com>
2019-01-26 15:37:33 +0100

    Add a stub for Prime

---
+++ src/Prime.java
@@ -0,0 +1,17 @@
+/**
+ * Created by mario on 26/01/19.
+ */
+public class Prime {
+    public static void main(String args[]) {
+        int num = 13;
+        if (isPrime(num)) {
+            System.out.println(num + "is Prime");
+        } else {
+            System.out.println(num + "is Not Prime");
+        }
+    }
+
+    public static Boolean isPrime(int num) {
+        return false;
+    }
+}
